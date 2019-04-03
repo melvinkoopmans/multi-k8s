690102683b5cc652ec0373f1c79d26b8dbe32cad
Melvin Koopmans <melvin.koopmans@gmail.com>
2019-04-03 21:58:42 +0200

    (feat): deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+docker build \
+    -t melvinkoopmans/multi-docker-client:latest \
+    -t melvinkoopmans/multi-docker-client:$GIT_SHA \
+    -f ./client/Dockerfile ./client
+
+docker build \
+    -t melvinkoopmans/multi-docker-server:latest \
+    -t melvinkoopmans/multi-docker-server:$GIT_SHA \
+    -f ./server/Dockerfile ./server
+
+docker build \
+    -t melvinkoopmans/multi-docker-worker:latest \
+    -t melvinkoopmans/multi-docker-worker:$GIT_SHA \
+    -f ./worker/Dockerfile ./worker
+
+docker push melvinkoopmans/multi-docker-client:lastest
+docker push melvinkoopmans/multi-docker-server:lastest
+docker push melvinkoopmans/multi-docker-worker:lastest
+
+docker push melvinkoopmans/multi-docker-client:$GIT_SHA
+docker push melvinkoopmans/multi-docker-server:$GIT_SHA
+docker push melvinkoopmans/multi-docker-worker:$GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=melvinkoopmans/multi-docker-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=melvinkoopmans/multi-docker-server:$GIT_SHA
+kubectl set image deployments/worker-deployment server=melvinkoopmans/multi-docker-worker:$GIT_SHA
