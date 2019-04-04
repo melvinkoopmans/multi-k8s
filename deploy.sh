14d5d1e7bcf2b1ab700bf9594758bbbff5921ddf
Melvin Koopmans <melvin.koopmans@gmail.com>
2019-04-04 22:20:12 +0200

    (fix): server first

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,15 @@ docker build \
     -t melvinkoopmans/multi-docker-worker:$GIT_SHA \
     -f ./worker/Dockerfile ./worker
 
-docker push melvinkoopmans/multi-docker-client:latest
 docker push melvinkoopmans/multi-docker-server:latest
+docker push melvinkoopmans/multi-docker-client:latest
 docker push melvinkoopmans/multi-docker-worker:latest
 
-docker push melvinkoopmans/multi-docker-client:$GIT_SHA
 docker push melvinkoopmans/multi-docker-server:$GIT_SHA
+docker push melvinkoopmans/multi-docker-client:$GIT_SHA
 docker push melvinkoopmans/multi-docker-worker:$GIT_SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment server=melvinkoopmans/multi-docker-client:$GIT_SHA
 kubectl set image deployments/server-deployment server=melvinkoopmans/multi-docker-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=melvinkoopmans/multi-docker-client:$GIT_SHA
 kubectl set image deployments/worker-deployment server=melvinkoopmans/multi-docker-worker:$GIT_SHA
